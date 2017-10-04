1fece9bfe862e91ca6bcefdc1300e62cde90ddb3
Mark van den Broek <mark@heyhoo.nl>
2017-10-04 17:25:33 +0200

    Fix attachment test.

--- tests/AttachmentTest.php
+++ tests/AttachmentTest.php
@@ -10,8 +10,9 @@ class AttachmentTest extends TestCase
     /** @test */
     public function attachment_is_constructed()
     {
-        $a = new Attachment('test.txt', 'plain/text', 8, 'dGVzdA==');
+        $a = new Attachment('test.txt', 'some-unqiue-id', 'plain/text', 8, 'dGVzdA==');
         $this->assertEquals($a->name, 'test.txt');
+        $this->assertEquals($a->contentId, 'some-unqiue-id');
         $this->assertEquals($a->contentType, 'plain/text');
         $this->assertEquals($a->contentLength, 8);
         $this->assertEquals($a->content(), 'test');
